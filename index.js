257b8580256e9223acd8b6269cdfb36b8dc1ab42
Dio <cdebon97@outlook.com>
2022-05-23 11:46:08 -0500

    Updated port address

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use('/api', requireAuth, defaultRouter)
 app.get('/tables', queries.setUpTables)
 
 
-const port = 8000;
+const port = processs.env.PORT || 8000;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port);
